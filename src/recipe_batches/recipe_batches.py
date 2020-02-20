172fc1c9060dfb62d9e1ba27b6c2596f79d2daec
Jack Seymour <stilljack@gmail.com>
2020-02-19 16:07:33 -0800

    recipe batches was a bit more straight forward, probably a better way

--- src/recipe_batches/recipe_batches.py
+++ src/recipe_batches/recipe_batches.py
@@ -2,13 +2,28 @@
 
 import math
 
-def recipe_batches(recipe, ingredients):
-  pass 
+def recipe_batches(recipe:dict, ingredients:dict):
+  #if mismatch of keys, return false
+  totalCount=[]
+  if recipe.keys()!=ingredients.keys():
+    return -0
+  for k,v in ingredients.items():
+    print(f"recipe v{recipe[k]} ingre v {v}")
+    #check how many we can make, i.e the value divided by how much we need for one recipe
+    res=v//recipe[k]
+    #appen to totalcount
+    totalCount.append(res)
+
+    ##csort should be fine
+  totalCount.sort()
+
+  return totalCount[0]
 
 
-if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+
+# if __name__ == '__main__':
+#   # Change the entries of these dictionaries to test
+#   # your implementation with different inputs
+#   recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
+#   ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
+#   print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
