a06d5e4bde15ef3c86624e2fd270f1fba2b791b7
Jack Seymour <stilljack@gmail.com>
2020-02-20 20:09:27 -0800

    so went about trying to making change wildly, wildly, the wrong way all day...
    the solution that I eventually found after a lot of research, well the fastest and most reliable solution is wildly, wildly, wildly faster then everything else... I think there's room to dissect this, and book recommendation it came with is something I've seen before Polya's "How to solve it".

--- src/making_change/making_change.py
+++ src/making_change/making_change.py
@@ -3,63 +3,124 @@
 import sys
 
 denominations = [1, 5, 10, 25, 50]
-bucket = {1:0,5:0,10:0,25:0,50:0}
-answer = {"answer":0}
-current_total=0
+bucket = {1: 0, 5: 0, 10: 0, 25: 0, 50: 0}
+answer = {"answer": 0}
+current_total = 0
+cache = {0: 0, 1: 1}
+
+#OK LETS TRY THE "RIGHT WAY TO DO IT"
+#HOLY CRAP THATS A BETTER SOLTUION!
+def making_change(total, denominations):
+    r = [1] + [0] * total
+    for k in denominations:
+        for i in range(k, len(r)):
+            r[i] += r[i - k]
+    return r[total]
+
+print(making_change(100, [50, 20, 10, 5, 1]))
+
+# WHY THE HELL DOESN'T THIS WORK????
+# def making_change(amount, denominations, cache={0: 1,1:1}):
+#     if amount == 1:
+#         return 1
+#     if amount == 0:
+#         return 1
+#     else:
+#         return rec_making_change(amount, denominations, cache)
+#
+#
+# def rec_making_change(amount, denominations, cache):
+#     if amount in cache:
+#         return cache[amount]
+#     elif amount < 0 or not denominations:
+#         return 0
+#
+#     coin = denominations[-1]
+#     return (cache[amount] = sum(
+#         rec_making_change(amt, denominations[:-1], cache)
+#         for amt in range(amount, -1, -coin)
+#     ))
 
 
-def making_change(amount,denominations,cache={0:1,1:1}):
-    def changer_recursive():
-        for entity in range():
+
+
+
+    #  print(f"cache {amount} recorded for {cache[amount]} ")
+
+
+#  return cache[amount]
+# this works:
+# def rec_making_change(amount, denominations,cache={0:1}):
+#     if amount in cache:
+#         return cache[amount]
+#     elif amount < 0 or not denominations:
+#         return 0
+#     else:
+#         coin = denominations[-1]
+#         return sum(
+#             rec_making_change(amt, denominations[:-1],cache)
+#             for amt in range(amount, -1, -coin)
+#         )
+
+print(making_change(12, [1, 5, 10, 25, 50]))
+
+
+def _ugg(amount, denominations, cache={0: 1, 1: 1}):
+    def changer_recursive(n, denominations):
+        for entity in range(len(denominations), 0, -1):
+            print(f"entity = {denominations[entity - 1]}")
             pass
+
     if amount < 0:
         return 0
+
     if amount not in cache:
-        for type in denominations:
-            changer_recursive(type)
+        for i in range(len(denominations), 0, -1):
+            print(denominations[i - 1])
+            changer_recursive(i, denominations)
 
-
+    answer[amount] = 0
     return answer[amount]
 
+
+# print(making_change(20,denominations))
+
 def helper_check(target):
-    current_total=0
-    current_total+=bucket[1]+\
-                   (bucket[5]*5)+\
-                   (bucket[10]*10)+\
-                   (bucket[25]*25)+\
-                   (bucket[50]*1)
+    current_total = 0
+    current_total += bucket[1] + \
+                     (bucket[5] * 5) + \
+                     (bucket[10] * 10) + \
+                     (bucket[25] * 25) + \
+                     (bucket[50] * 1)
     print(f"current_total {current_total}")
-    if target%current_total == 0:
+    if target % current_total == 0:
         print(f"answer increased by one for\n bucker = {bucket}")
-        answer["answer"]+=1
+        answer["answer"] += 1
+    return answer["answer"]
 
 
-def making_change(amount, denominations,cache={0:1,1:1}):
+def making_change_old(amount, denominations, cache={0: 1, 1: 1}):
     if amount < 0:
         return 0
     if amount not in cache:
-        for half_dollars in range(0,amount,50):
-            for quarter in range(0,amount,25):
-                for dimes in range(0,amount,10):
-                    for nickles in range(0,amount,5):
+        for half_dollars in range(0, amount, 50):
+            for quarter in range(0, amount, 25):
+                for dimes in range(0, amount, 10):
+                    for nickles in range(0, amount, 5):
                         for pennies in range(amount):
-                            bucket[1]+=1
+                            bucket[1] += 1
                             helper_check(amount)
-                        bucket[5]+=1
+                        bucket[5] += 1
                         helper_check(amount)
-                    bucket[10]+=1
+                    bucket[10] += 1
                     helper_check(amount)
-                bucket[25]+=1
+                bucket[25] += 1
                 helper_check(amount)
-            bucket[50]+=1
+            bucket[50] += 1
             print(f"bucket={bucket}")
             helper_check(amount)
-    return(answer["answer"])
-
-
-
-print(making_change(20,denominations))
-
+        cache[amount] = current_total
+    return answer["answer"]
 
 # if __name__ == "__main__":
 #   # Test our your implementation from the command line
--- src/making_change/test_making_change.py
+++ src/making_change/test_making_change.py
@@ -4,28 +4,53 @@ from src.making_change.making_change import making_change
 
 class Test(unittest.TestCase):
 
-  def setUp(self):
-    self.denominations = [1, 5, 10, 25, 50]
+    def setUp(self):
+        self.denominations = [1, 5, 10, 25, 50]
 
-  def test_making_change_small_amount(self):
-    self.assertEqual(making_change(0, self.denominations), 1)
-    self.assertEqual(making_change(1, self.denominations), 1)
-    self.assertEqual(making_change(5, self.denominations), 2)
-    self.assertEqual(making_change(10, self.denominations), 4)
-    self.assertEqual(making_change(20, self.denominations), 9)
-    self.assertEqual(making_change(30, self.denominations), 18)
-    self.assertEqual(making_change(100, self.denominations), 292)
-    self.assertEqual(making_change(200, self.denominations), 2435)
-    self.assertEqual(making_change(300, self.denominations), 9590)
+    def test_making_change_small_amount(self):
+         self.assertEqual(making_change(0, self.denominations), 1)
 
-  def test_making_change_large_amount(self):
-    self.assertEqual(making_change(350, self.denominations), 16472)
-    self.assertEqual(making_change(400, self.denominations), 26517)
-    self.assertEqual(making_change(1000, self.denominations), 801451)
-    self.assertEqual(making_change(2000, self.denominations), 11712101)
-    self.assertEqual(making_change(5000, self.denominations), 432699251)
-    self.assertEqual(making_change(10000, self.denominations), 6794128501)
+    def test_making_change_small_amount1(self):
+         self.assertEqual(making_change(1, self.denominations), 1)
+
+    def test_making_change_small_amount2(self):
+        self.assertEqual(making_change(5, self.denominations), 2)
+
+    def test_making_change_small_amount4(self):
+        self.assertEqual(making_change(10, self.denominations), 4)
+
+    def test_making_change_small_amount5(self):
+        self.assertEqual(making_change(20, self.denominations), 9)
+
+    def test_making_change_small_amount30(self):
+        self.assertEqual(making_change(30, self.denominations), 18)
+
+    def test_making_change_small_amount292(self):
+        self.assertEqual(making_change(100, self.denominations), 292)
+
+    def test_making_change_small_amount2435(self):
+        self.assertEqual(making_change(200, self.denominations), 2435)
+
+    def test_making_change_small_amount9590(self):
+        self.assertEqual(making_change(300, self.denominations), 9590)
+
+    def test_making_change_large_amount1(self):
+        self.assertEqual(making_change(350, self.denominations), 16472)
+
+    def test_making_change_large_amount2(self):
+        self.assertEqual(making_change(400, self.denominations), 26517)
+
+    def test_making_change_large_amount3(self):
+        self.assertEqual(making_change(1000, self.denominations), 801451)
+
+    def test_making_change_large_amount4(self):
+        self.assertEqual(making_change(2000, self.denominations), 11712101)
+
+    def test_making_change_large_amount5(self):
+        self.assertEqual(making_change(5000, self.denominations), 432699251)
+    def test_making_change_large_amount6(self):
+        self.assertEqual(making_change(10000, self.denominations), 6794128501)
 
 
 if __name__ == '__main__':
-  unittest.main()
+    unittest.main()
